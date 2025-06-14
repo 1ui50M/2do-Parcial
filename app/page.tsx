835cd4189abb6e96a6f7b28e008a8152f71c6a16
1ui50M <delontiverosm@gmail.com>
2025-06-14 09:59:49 -0400

    otro

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,23 @@ import React from 'react'
 import Header from '@/Header/Header'
 import About from '@/About/About'
 import Dashboard from '@/Dashboard/Dashboard'
+import Link from 'next/link'
 
 
 const page = () => {
   return (
     <>
-    <Home/>
-    <Header/>
-    <About/>
-    <Dashboard/>
+    <div className='todo'>
+      <Link href="./Home">
+        <h1>Home</h1>
+      </Link>
+      <Link href="/About">
+        <h1>About</h1>
+      </Link>
+      <Link href="/Dashboard">
+        <h1>Dashboard</h1>
+      </Link> 
+    </div>
     </>
   )
 }
